fe83a6cf0dc7710d88f211c7ea5ab408c25288c5
Giovanna de Freitas <giovannadefreitasaraujo@hotmail.com>
2025-01-23 19:48:01 -0300

    add zod validation and show errors on form

--- src/components/organisms/ProductForm.tsx
+++ src/components/organisms/ProductForm.tsx
@@ -1,33 +1,38 @@
 "use client"
 import postCreateProduct from "@/services/api/postCreateProduct"
+import { zodResolver } from "@hookform/resolvers/zod"
 import MDEditor from "@uiw/react-md-editor"
-import { useForm } from "react-hook-form"
+import { Controller, useForm } from "react-hook-form"
 import toast from "react-hot-toast"
 import * as z from 'zod'
-interface Inputs {
-    name: string,
-    value: number
-}
+
+const formSchema = z.object({
+    name: z.string().min(1, 'Campo obrigatório'),
+    value: z.coerce.number().min(1, 'O valor precisa ser maior que zero'),
+    description: z.string().min(1, 'Campo obrigatório'),
+    image: z.string().min(1, 'Campo obrigatório')
+})
+
+type Schema = z.infer<typeof formSchema>
 
 const ProductForm = () => {
-    const formSchema = z.object({
-        name: z.string()
-    })
-
-    const { register, handleSubmit, formState: { errors } } = useForm<Inputs>({
+    const { register, handleSubmit, control, formState: { errors } } = useForm<Schema>({
+        resolver: zodResolver(formSchema),
         defaultValues: {
             name: "",
-            value: 0
+            value: 0,
+            description: "",
+            image: ""
         },
     })
 
-    const submitForm = async (data: Inputs) => {
+    const submitForm = async (data: Schema) => {
         try {
             const createdProduct = await postCreateProduct({
                 name: data.name,
-                description: "knsdckjn",
+                description: data.description,
                 value: data.value,
-                image: "dasad"
+                image: data.image
             })
             toast.success('Produto criado')
         } catch (error) {
@@ -41,13 +46,35 @@ const ProductForm = () => {
 
     return (
         <form onSubmit={handleSubmit(submitForm)} className="w-4/5 bg-white mt-10 rounded-md p-6 flex flex-col gap-3">
-            <p className="text-xl font-semibold">Nome e descrição</p>
-            <label className="text-sm" htmlFor="name">Nome</label>
+            <p className="text-xl font-semibold">Detalhes</p>
+
+            {/* name input */}
+            <label className="text-sm font-semibold" htmlFor="name">Nome</label>
             <input className="h-8 rounded-md border border-[#273056] p-2" {...register("name", { required: "Campo obrigatório" })} type="text" />
-            <label className="text-sm" htmlFor="value">Valor</label>
-            <input className="h-8 rounded-md border border-[#273056] p-2" {...register("value", { required: "Campo obrigatório" })} type="text" />
-            {/* <MDEditor  /> */}
-            <button type="submit" className="w-fit flex items-center gap-2 bg-[#273056] px-4 py-2 rounded-md text-white font-medium">Salvar alterações</button>
+            {errors.name && <span className="text-sm text-red-700 font-semibold">{errors.name.message}</span>}
+
+            {/* value input */}
+            <label className="text-sm font-semibold" htmlFor="value">Valor</label>
+            <input className="h-8 rounded-md border border-[#273056] p-2" {...register("value", { required: "Campo obrigatório" })} type="number" />
+            {errors.value && <span className="text-sm text-red-700 font-semibold">{errors.value.message}</span>}
+
+            {/* image input */}
+            <label className="text-sm font-semibold" htmlFor="image">Imagem</label>
+            <input className="h-8 rounded-md border border-[#273056] p-2" {...register("image", { required: "Campo obrigatório" })} type="text" />
+            {errors.image && <span className="text-sm text-red-700 font-semibold">{errors.image.message}</span>}
+
+            {/* description input */}
+            <p className="text-sm font-semibold">Descrição</p>
+            <Controller
+                name="description"
+                control={control}
+                rules={{ required: true }}
+                render={({ field }) => <MDEditor preview="edit" value={field.value} onChange={field.onChange} />
+                }
+            />
+            {errors.description && <span className="text-sm text-red-700 font-semibold">{errors.description.message}</span>}
+
+            <button type="submit" className="mt-4 w-fit flex items-center gap-2 bg-[#273056] px-4 py-2 rounded-md text-white font-medium">Salvar alterações</button>
         </form>
     )
 }
