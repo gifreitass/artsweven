9b3716395582b6867b62bf7d9361561e7f2de8fb
Giovanna de Freitas <giovannadefreitasaraujo@hotmail.com>
2025-01-28 17:49:11 -0300

    add map to list products

--- src/components/organisms/ProductList.tsx
+++ src/components/organisms/ProductList.tsx
@@ -1,11 +1,17 @@
+"use client"
 import ProductCard from "../molecules/ProductCard"
+import useProductList from "@/hooks/useProductList"
 
 const ProductList = () => {
+    const { products, loading } = useProductList()
+
+    console.log(products)
+
     return (
         <div className="flex flex-col gap-7 mt-10 w-4/5">
-            {/* map para gerar um card por produto */}
-            <ProductCard />
-            <ProductCard />
+            {products && products.map((product, index) => {
+                return (<ProductCard image={product.image} name={product.name} value={product.value} key={index} />)
+            })}
         </div>
     )
 }
