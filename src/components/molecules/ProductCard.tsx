713e39ca1577c1d3b41a67a159337d3b6747c5f0
Giovanna de Freitas <giovannadefreitasaraujo@hotmail.com>
2025-01-28 17:47:40 -0300

    add props to product card

--- src/components/molecules/ProductCard.tsx
+++ src/components/molecules/ProductCard.tsx
@@ -1,17 +1,19 @@
 import Link from "next/link"
 
-const ProductCard = () => {
+const ProductCard: React.FC<{ name: string, value: number, image: string }> = (props) => {
+    const { name, value, image } = props
+            
     return (
         <div className="bg-white rounded-lg px-7 py-5 flex justify-between items-center">
             <div className="flex gap-6 items-center">
-                <img className="w-20 h-20 rounded-md shadow-md" src="https://acdn.mitiendanube.com/stores/001/661/511/products/version1uuid87ec0c9f-cc1c-4045-9554-452e0e6550dfmodecompatiblenoloc01-f1a926acdbe5d5941b16838533684401-1024-1024.webp" alt="product" />
+                <img className="w-20 h-20 rounded-md shadow-md" src={image} alt="product" />
                 <div>
-                    <p className="font-medium">luminária affection</p>
-                    <p>R$80</p>
+                    <p className="font-medium">{name}</p>
+                    <p>R${value}</p>
                 </div>
             </div>
             <div>
-                <Link href="/backoffice/products/id"><img className="w-7 cursor-pointer" src="/images/editing.png" alt="edit icon" /></Link>
+                <Link href="/backoffice/products/id"><img className="w-7 cursor-pointer" src={"/images/editing.png"} alt="edit icon" /></Link>
             </div>
         </div>
     )
