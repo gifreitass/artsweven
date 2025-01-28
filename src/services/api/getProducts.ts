ae7afc914d241ef1218e79935de3c4aaef793596
Giovanna de Freitas <giovannadefreitasaraujo@hotmail.com>
2025-01-28 17:48:47 -0300

    create get product service

---
+++ src/services/api/getProducts.ts
@@ -0,0 +1,13 @@
+const getProducts = async () => {
+    const response = await fetch("http://localhost:3001/product")
+
+    const result = await response.json()
+
+    if (!response.ok) {
+        throw new Error(result.error.message)
+    }
+
+    return result.data
+}
+
+export default getProducts
